f55864dd90446bbe20c19fc50f9e07de80d0f385
Ayush Satvara <ayushsatvara@MacBookPro.lan>
2026-02-11 18:46:04 -0500

    scroll restoration

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import './index.css'
 function App() {
   return (
     <div className="flex flex-col">
-      {/* <ScrollRestoration /> */}
+      <ScrollRestoration />
       <Navbar />
         <Outlet />
       <Footer />
